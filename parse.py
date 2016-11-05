55366cb69b1431a7c7dcc9926018086f964f9a3a
Isaiah Mann <contact@isaiahmann.com>
2016-11-04 21:29:01 -0400

    Removing extra new line char

--- parse.py
+++ parse.py
@@ -7,6 +7,6 @@ with open('twitterfeed.json') as data_file:
     data = json.load(data_file)[twitterKey]
 tweets = {}
 for i in range(0, len(data)):
-    tweets[data[i][userKey][nameKey]] = data[i][textKey] + '\n'
+    tweets[data[i][userKey][nameKey]] = data[i][textKey]
 with open('twitterfeedexport.json', 'w') as fp:
     json.dump(tweets, fp, indent=2)
