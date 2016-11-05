693c68b3430b0b539565c4ed5c602eb3e0780d85
Isaiah Mann <contact@isaiahmann.com>
2016-11-04 21:28:11 -0400

    Adding script to parse out just the user name and the text of tweets

---
+++ parse.py
@@ -0,0 +1,12 @@
+import json
+textKey = "text"
+userKey = "user"
+nameKey = "name"
+twitterKey = "Twitter"
+with open('twitterfeed.json') as data_file:
+    data = json.load(data_file)[twitterKey]
+tweets = {}
+for i in range(0, len(data)):
+    tweets[data[i][userKey][nameKey]] = data[i][textKey] + '\n'
+with open('twitterfeedexport.json', 'w') as fp:
+    json.dump(tweets, fp, indent=2)
